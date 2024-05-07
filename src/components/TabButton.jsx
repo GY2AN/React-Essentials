58363037f73128daafbe024733fc789fb721a310
GyanCoverQ <gyan@coverq.io>
2024-05-07 16:58:39 +0530

    second commit

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,9 @@
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
     console.log("TabContent Component Execting")
     
     return (
         <li>
-            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
         </li>
     );
 }
